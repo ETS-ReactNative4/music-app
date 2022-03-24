adff13f95ad1d7227b50ccc1da76ed86d6b1f216
kattai <satanregrets@gmail.com>
2022-03-24 19:54:42 -0300

    still no music pause function just playing

--- App.js
+++ App.js
@@ -1,11 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av';
+import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons'; 
 
 export default function App() {
+  const [audio, setAudio] = useState(null);
+  const [songs, setSongs] = useState([
+    {
+      name: 'Shine on You Crazy',
+      artists: 'Pink Floyd',
+      playing: false,
+      file: require('./songs/music.mp3')
+    },
+    {
+      name: 'Killer Queen',
+      artists: 'Queen',
+      playing: false,
+      file: require('./songs/music.mp3')
+    },
+    {
+      name: 'Stairway to Heaven',
+      artists: 'Led Zeppelin',
+      playing: false,
+      file: require('./songs/music.mp3')
+    },
+  ]);
+
+  const changeSong = async (id) => {
+    let currentFile = null;
+    let newSong = songs.filter((song, i) => {
+      if(id === i){
+        songs[i].playing = true;
+        currentFile = songs[i].file;
+      }else{
+        songs[i].playing = false;
+      }
+      return songs[i];
+    });
+    if(audio != null){
+      audio.unloadAsync();
+    }
+    let currentSong = new Audio.Sound();
+    try{
+      await currentSong.loadAsync(currentFile);
+      await currentSong.playAsync();
+    }catch(e){}
+    setAudio(currentSong);
+    setSongs(newSong);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+    <ScrollView style={styles.container}>
+      <StatusBar hidden/>
+      <View style={styles.header}>
+        <Text style={{textAlign: 'center', color: '#fff', fontSize: 30}}>NightMusic</Text>
+      </View>
+      <View style={styles.table}>
+        <Text style={styles.tableText}>Música</Text>
+        <Text style={styles.tableText}>Artista</Text>
+      </View>
+      {
+        songs.map((song, i) => {
+          if(song.playing){
+            return (
+              <View key={song.name} style={styles.table}>
+                <TouchableOpacity onPress={() => changeSong(i)} style={styles.tableSong}>
+                <Text style={{
+                  marginRight:10
+                }}> <Ionicons name="md-pause" size={17} color="#6619ff" /></Text>
+                  <Text style={styles.tableSongTextPlaying}>{song.name}</Text>
+                  <Text style={styles.tableSongTextPlaying}> - </Text>
+                  <Text style={styles.tableSongTextPlaying}>{song.artists}</Text>
+                </TouchableOpacity>                 
+              </View>
+            )
+          }else{
+            return(
+              <View  key={song.name} style={styles.table}>
+              <TouchableOpacity onPress={() => changeSong(i)} style={styles.tableSong}>
+                <Text style={{
+                  marginRight:10
+                }}><Ionicons name="md-play" size={17} color="white" /></Text>
+                <Text style={styles.tableSongText}>{song.name}</Text>
+                <Text style={styles.tableSongText}> - </Text>
+                <Text style={styles.tableSongText}>{song.artists}</Text>
+              </TouchableOpacity>
+            </View>
+            )
+          }
+        })
+          
+        }
+    </ScrollView>
     </View>
   );
 }
@@ -13,8 +101,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#161616',
   },
+  header: {
+    backgroundColor: '#5500ff',
+    width: '100%',
+    padding: 20
+  },
+  table: {
+    flexDirection: 'row',
+    padding: 20,
+    borderBottomColor: '#111',
+    borderBottomWidth: 2
+  },
+  tableText:{
+    paddingLeft: 5,
+    color: '#fff', 
+    width: '50%',
+    fontSize:17
+  },
+  tableSong: {
+    alignItems: 'center',
+    width: '100%',
+    flexDirection: 'row'
+  },
+  tableSongText:{
+    paddingLeft: 5,
+    color: '#fff', 
+    fontSize:17
+  },
+  tableSongTextPlaying: {
+    paddingLeft: 5,
+    color: '#6619ff', 
+    fontSize:17
+  }
 });
